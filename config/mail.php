8357e1473be36c2543b7fe7b954e8d5ffac4eab1
Dmitry <dmitrii_mv4@mail.ru>
2025-04-18 15:24:58 +0300

    option mail

--- config/mail.php
+++ config/mail.php
@@ -41,12 +41,12 @@ return [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => env('MAIL_HOST', 'mail.timeweb.com'),
+            'port' => env('MAIL_PORT', 465),
+            'username' => env('pulsespace@dmv4.ru'),
+            'password' => env('K09412zGa'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', Request::root()), PHP_URL_HOST)),
         ],
 
         'ses' => [
